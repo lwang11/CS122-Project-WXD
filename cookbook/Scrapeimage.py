b907f2b2e4c9480a03edef3007bd80ff6c217220
lwang55 <lwang55@u.rochester.edu>
2018-03-13 10:04:20 -0500

    update cookbook with comments

--- cookbook/Scrapeimage.py
+++ cookbook/Scrapeimage.py
@@ -1,3 +1,4 @@
+# Modified
 # The codes below are "modified" and learned online from https://gist.github.com/genekogan/ebd77196e4bf0705db51f86431099e57
 
 import argparse
@@ -15,7 +16,9 @@ import requests
 from bs4 import BeautifulSoup
 
 def configure_logging():
-    # "Direct copy", learned from online
+    '''
+    create a logger
+    '''
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
@@ -30,30 +33,45 @@ REQUEST_HEADER = {
     'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
 
 def get_query_url(query):
-    # "original", get url from a input query using encode specific for google
+    '''
+    get url from a input query using encode specific for google
+    Input:
+       query: string
+    '''
     qq = urllib.parse.quote(query)
     return "https://www.google.co.in/search?q=%s&source=lnms&tbm=isch" % qq
 
 def get_soup(url, header):
-    # "original", get soup from an url
+    '''
+    get soup from an url and header
+    Input: url, header
+    Return: soup object
+    '''
     response = urlopen(Request(url, headers=header))
     return BeautifulSoup(response, 'html.parser')
 
 def extract_images_from_soup(soup):
-    # "direct copy", extract image from the soup
+    '''
+    extract image from the soup
+    '''
     image= soup.find_all("div", {"class": "rg_meta"})
     metadata_dict = (json.loads(e.text) for e in image)
     link_type_records = ((d["ou"], d["ity"]) for d in metadata_dict)
     return link_type_records
 
-def extract_images(query, num_images):
-    # "modified", extract image given an input query and number of images
-    # and return the link of this image
+def extract_images(query):
+    '''
+    extract image given an input query 
+    and return the url of this image
+    '''
     url = get_query_url(query)
     logger.info("Souping")
     soup = get_soup(url, REQUEST_HEADER)
     logger.info("Extracting image urls")
     link_type_records = extract_images_from_soup(soup)
+    # we tried some queries, and we found that the first image is 
+    # sometimes  not related to food, such as vanilla snow, which is
+    # also a song, so we scrape the second image of each recipe
     return list(itertools.islice(link_type_records, 2))[1][0]
 
 
--- cookbook/search/views.py
+++ cookbook/search/views.py
@@ -1,3 +1,4 @@
+# Original/Heavily modified
 
 import json
 import traceback
@@ -18,6 +19,7 @@ NOPREF_STR = 'No preference'
 RES_DIR = os.path.join(os.path.dirname(__file__), '..', 'res')
 
 
+# create a searchform class with the form fields we need
 class SearchForm(forms.Form):
     query = forms.CharField(
         label='Ingredients',
@@ -30,9 +32,15 @@ class SearchForm(forms.Form):
                                    help_text='e.g. onion', 
                                    required=False)
 
-    
 
 def home(request):
+    '''
+    Process the request; Extracts user's query and  process it, 
+    convert the input query into variables in our existing python 
+    function. Combines a variables we used with a given context 
+    dictionary and returns an HttpResponse object with that rendered text.
+    '''
+
     result=None
     food_query = None
     img_url = None
@@ -47,11 +55,10 @@ def home(request):
 
             result = find_recipe('full_format_recipes.json', food_query, num_char, without,
                                  load_index_in_json, load_documents, load_inverted_index, load_doc_length)
-               
 
             res = []
             for i in result:
-                img_url = extract_images(i[0], 1)
+                img_url = extract_images(i[0])
                 res.append((i[0], i[1], i[2], img_url))
 
            
